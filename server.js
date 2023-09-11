3c1e385964eb88fc2ee12d35c0ac8db65abcdea9
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-09-11 11:46:36 -0500

    fix: naming

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 
-const routes = require('./routes');
+const controllers = require('./controllers');
 
 const sequelize = require('./config/connection');
 
@@ -20,7 +20,7 @@ app.use(session(sessionOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use(controllers);
 
 // Force true to drop/recreate table(s) on every sync
 sequelize.sync({ force: true }).then(() => {
